5879e7978cc528055db895cfb1e9c94741d59a7c
TranNhi27 <18521208@gm.uit.edu.vn>
2023-12-11 21:41:01 +0700

    add Copy Address

--- src/components/EventPreview.js
+++ src/components/EventPreview.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Subscribe from "./Subscribe";
 import { Link } from "react-router-dom";
 import { claimLucky } from "../helpers/social";
+import { parseAddress, parseAmount } from "../libs/blockchain";
 
 const EventPreview = ({
 	tag,
@@ -32,6 +33,21 @@ const EventPreview = ({
 
 	const [loading, setLoading] = useState(false);
 	const [success, setSuccess] = useState(true);
+	const [copy, setCopy] = useState("click to copy");
+
+
+	const resetTooltip = () => {
+		setTimeout(() => {
+			setCopy("Copy Address");
+		}, 50);
+		document.removeEventListener("mouseout", resetTooltip);
+	};
+	const copyText = (text) => {
+		navigator.clipboard.writeText(text);
+		setCopy("Add to LINK VRF");
+		document.addEventListener("mouseout", resetTooltip);
+	};
+
 
 	const claim = async () => {
 		setLoading(true);
@@ -47,17 +63,28 @@ const EventPreview = ({
 			{subscribePopup && (
 				<Subscribe user={owner} setSubscribePopup={setSubscribePopup} />
 			)}
-			<Link
-				className="mx-4 w-3/4 flex flex-col space-y-3 content-between container"
-				to={`/profile/${owner.id}`}
-			>
-				<div
-					className={`grid text-gray-900 hover:text-gray-500 cursor-pointer`}
+			<div>
+				<Link
+					className="mx-4 w-3/4 flex flex-col space-y-3 content-between container"
+					to={`/profile/${owner.id}`}
 				>
-					<p className="font-serif text-xl font-bold truncate">{header()}</p>
-					<p className="text-sm mt-2 font-light">by {owner.name}</p>
+					<div
+						className={`grid text-gray-900 hover:text-gray-500 cursor-pointer`}
+					>
+						<p className="font-serif text-xl font-bold truncate">{header()}</p>
+						<p className="text-sm mt-2 font-light">by {owner.name}</p>
+					</div>
+				</Link>
+				<div
+					className="group relative border border-gray-200 hover:border-gray-300 col-span-3 hover:bg-gray-200 hover:cursor-pointer py-2 pr-4 hover:text-gray-800 w-full h-min flex justify-between"
+					onClick={() => copyText(addressCollection)}
+				>
+					<p className="text-lg mx-4">{parseAddress(addressCollection)}</p>
+					<span className="self-center pointer-events-none opacity-0 font-semibold transition-opacity group-hover:opacity-100">
+						{copy}
+					</span>
 				</div>
-			</Link>
+			</div>
 			<div className="w-1/4 items-center place-items-center flex h-full border-l px-4 ml-4 border-gray-600">
 				{loading && !success ? (
 					<div className="animate-spin rounded-full h-6 w-6 border-b-2 border-gray-500"></div>
